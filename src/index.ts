41afa34d1afb0de1c4d111cd0837c29eefd3230f
Eoin Traynor <eointraynor1@gmail.com>
2024-07-24 00:24:35 +0200

    Store env variables, check logged in status, try capture response

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,13 @@ const initBrowser = async () => {
   return browser;
 };
 
-const username = '';
-const password = '';
+const username = process.env.EMAIL;
+const password = process.env.PASSWORD;
+const baseUrl = 'https://tickets.union-zeughaus.de';
+const matchUrlPath = '1.-fc-union-berlin-glasgow-rangers.htm'
 
 const login = async (page: puppeteer.Page) => {
+  if (!password || !username) return Error('No username or password found');
   await page.goto("https://anmeldung.fc-union-berlin.de/auth/realms/union/protocol/openid-connect/auth?client_id=union-tickets&scope=openid+lms_id&redirect_uri=https%3a%2f%2ftickets.union-zeughaus.de%2funveu%2fSynwayLoginSso%2fLoginCallback%2fAnmeldung&response_type=code");
   await page.type("#username", username);
   await page.type("#password", password);
@@ -20,22 +23,64 @@ const login = async (page: puppeteer.Page) => {
 };
 
 const navigateToGame = async (page: puppeteer.Page) => {
-  await page.goto('https://tickets.union-zeughaus.de/unveu/1.-fc-union-berlin-rasenballsport-leipzig.htm');
+  await page.goto(`${baseUrl}/unveu/${matchUrlPath}`);
   if (await page.waitForSelector('#CybotCookiebotDialogBodyButtonDecline')) {
     await page.click('#CybotCookiebotDialogBodyButtonDecline');
   }
   await page.waitForNavigation();
 }
 
-const findTicket = async (page: puppeteer.Page) => {
-  await page.waitForSelector('.jsVenue');
-
+const isLoggedIn = async (page: puppeteer.Page) => {
+  try {
+    await page.waitForSelector(`[href='SynwayLoginSso/Logout/Anmeldung']`)
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
+
 
 (async () => {
   const browser = await initBrowser();
   const page = await browser.newPage();
+
   await login(page);
-  await navigateToGame(page);
+
+  if (await isLoggedIn(page)) {
+
+    await page.setRequestInterception(true);
+    // page.on('request', async request => {
+    //   const url = request.url();
+    //   if (url.startsWith('https://tickets.union-zeughaus.de/unveu/SynwayVenue/Prices/Veranstaltungen/')) {
+    //     const response = await request.response();
+    //     console.log(response);
+    //   }
+    //   request.continue();
+    // })
+
+    // page.on('requestfinished', async (request) => {
+    //   if (request.url().includes('unveu/SynwayVenue/Prices/Veranstaltungen')){
+    //     const response = await request.response();
+    //     if (response) {
+    //       const headers = response.headers();
+    //       console.log({headers});
+    //     }
+    //   }
+    //   request.continue()
+    // });
+
+    // page.on('response', response => {
+    //   const url = response.url();
+    //   console.log(url);
+    //   if (url.startsWith('https://tickets.union-zeughaus.de/unveu/SynwayVenue/Prices/Veranstaltungen/')) {
+    //     const headers = response.headers();
+    //     const json = response.json();
+    //     const postData = response.request().postData();
+    //     console.log({url, headers, json, postData});
+    //   }
+    // });
+
+    await navigateToGame(page);
+  }
   await browser.close();
 })();
