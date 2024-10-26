e51f418c9cddbdc33a8a55b406f5073f5d6ab3ee
Eoin Traynor <eointraynor1@gmail.com>
2024-10-26 23:56:31 +0200

    Begin work on reload of page with zero results

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const initBrowser = async () => {
   const browser = await puppeteer.launch({
     headless: false,
     slowMo: 50,
+    devtools: true,
   });
   return browser;
 };
@@ -12,8 +13,8 @@ const initBrowser = async () => {
 const username = process.env.EMAIL;
 const password = process.env.PASSWORD;
 const baseUrl = 'https://tickets.union-zeughaus.de';
-const matchUrlPath = 'Veranstaltungen/e51be730-d9cb-4ff7-a1e9-29c3203a2f20';
-// const matchId = '534be454-f064-42f4-a434-8530d8ad4a48';
+const matchUrlPath = 'Veranstaltungen2/4312d7e8-7a69-4982-b6fb-29e315ef1fd5';
+// const matchUrlPath = 'Veranstaltungen/e51be730-d9cb-4ff7-a1e9-29c3203a2f20';
 
 declare global {
   interface Window {
@@ -64,18 +65,21 @@ type Price = {
 
 const login = async (page: puppeteer.Page) => {
   if (!password || !username) return Error('No username or password found');
-  await page.goto("https://anmeldung.fc-union-berlin.de/auth/realms/union/protocol/openid-connect/auth?client_id=union-tickets&scope=openid+lms_id&redirect_uri=https%3a%2f%2ftickets.union-zeughaus.de%2funveu%2fSynwayLoginSso%2fLoginCallback%2fAnmeldung&response_type=code");
+  await page.goto(`${baseUrl}/unveu/data?RedirectToSso=1`);
   await page.type("#username", username);
   await page.type("#password", password);
   await page.click('.form__submit-button');
-  await page.waitForNavigation();
+  // await page.waitForNavigation();
 };
 
-const navigateToGame = async (page: puppeteer.Page) => {
-  await page.goto(`${baseUrl}/unveu/venue/${matchUrlPath}`, { waitUntil: 'domcontentloaded' });
+const declineCookies = async (page: puppeteer.Page) => {
   if (await page.waitForSelector('#CybotCookiebotDialogBodyButtonDecline')) {
     await page.click('#CybotCookiebotDialogBodyButtonDecline');
   }
+}
+
+const navigateToGame = async (page: puppeteer.Page) => {
+  await page.goto(`${baseUrl}/unveu/venue/${matchUrlPath}`, { waitUntil: 'domcontentloaded' });
 }
 
 const isLoggedIn = async (page: puppeteer.Page) => {
@@ -87,49 +91,30 @@ const isLoggedIn = async (page: puppeteer.Page) => {
   }
 }
 
-(async () => {
-  const browser = await initBrowser();
-  const page = await browser.newPage();
+const attemptTpBook = async (page: puppeteer.Page) => {
+  await page.waitForFunction(`
+    Object.keys(
+      document.querySelector("canvas")
+    ).some(k => k.startsWith("jQuery"))
+  `);
 
-  // await login(page)
-  // if (await isLoggedIn(page)) {
-  //
-  // }
-    const availableTickets: AvailableTicket[] = [];
-    page.on('response', async response => {
-      const url = await response.url();
-      if (url.startsWith('https://tickets.union-zeughaus.de/unveu/SynwayVenue/Prices/Veranstaltungen')) {
-        const json = await response.json();
-        const jsonData: AvailableTicket = json.data;
-        const isSeatedSection = () => jsonData.BlockName === null;
-        const isAwaySection = () => jsonData.BlockName.includes('SEKTOR 5');
-        if (!isSeatedSection() && !isAwaySection()) {
-          availableTickets.push(jsonData);
-        }
+  const data = await page.$eval("canvas", async el => {
+    const jQuery =
+    // @ts-expect-error fsaldj
+      Object.entries(el).find(([k, v]) => {
+        return k.startsWith("jQuery")
       }
-    });
-
-    await navigateToGame(page);
-
-    await page.waitForFunction(`
-      Object.keys(
-        document.querySelector("canvas")
-      ).some(k => k.startsWith("jQuery"))
-    `);
-
-    const data = await page.$eval("canvas", async el => {
-      const jQuery =
-      // @ts-expect-error fsaldj
-        Object.entries(el).find(([k, v]) => {
-          return k.startsWith("jQuery")
-        }
-        )[1];
-      const { venue } = jQuery;
-      const { SubName, VID, BookTicket } = venue;
-      const { Blocks = [] } = venue.Venue;
-      const bookableBlocks = Blocks.filter((block: {Blocked: boolean }) => block.Blocked === false);
-      // Background color may indicate if it's possible to purchase a ticket in that block.
-      const { CurrentResellingId, ID, BackgroundColor, FullName, ShortName} = bookableBlocks[0];
+      )[1];
+    const { venue } = jQuery;
+    const { SubName, VID, BookTicket } = venue;
+    const { Blocks = [] } = venue.Venue;
+    // return Blocks;
+    const suitableBlocks = ["SEKTOR 2", "SEKTOR 3", "SEKTOR 4"]
+    const bookableBlocks = Blocks.filter((block: {Blocked: boolean, Stand: string }) => block.Blocked === false && suitableBlocks.includes(block.Stand));
+    // const bookableBlocks = bookableBlockss.slice(8);
+    // Background color may indicate if it's possible to purchase a ticket in that block.
+    for (let block = 0; block < bookableBlocks.length; block++) {
+      const { CurrentResellingId, ID, BackgroundColor, FullName, ShortName } = bookableBlocks[block];
       const payload = {
         "Count": 1,
         "BlockID": ID, // BlockID
@@ -139,11 +124,47 @@ const isLoggedIn = async (page: puppeteer.Page) => {
         "SubName": SubName,
       }
       await jQuery.venue.BookTicket(payload)
+    }
 
-      return { payload, BookTicket: jQuery.venue.BookTicket };
-    });
+    return bookableBlocks;
+  });
+  return data;
+}
 
-    console.log(data);
+(async () => {
+  const browser = await initBrowser();
+  const page = await browser.newPage();
 
+  await login(page);
+  await declineCookies(page);
+  await navigateToGame(page);
+
+  const isCanvasPresent = await page.waitForSelector('canvas');
+  if (!isCanvasPresent) {
+    console.log('reload');
+    page.reload();
+  }
+
+  const data = await attemptTpBook(page);
+  console.log({data});
+
+    if (!data.length) {
+      await page.reload({ waitUntil: "networkidle2"});
+      await page.waitForSelector('booooo');
+    }
+
+    // await page.reload({ waitUntil: "networkidle2"});
+
+    // if (await page.waitForSelector('#jsShoppingCartButtonCount')) {
+    //   await page.click('#jsShoppingCartButtonCount');
+    // }
+
+    // if (await page.waitForSelector('.jsVenueShoppingCart')) {
+    //   const shoppingCartTickets = await page.$$('.shoppingcart-item-ticket');
+    //   console.log(shoppingCartTickets);
+
+    // }
+
+    // await page.waitForSelector('#CybotCookiebotDialogBodyButtonDeclinefdasf');
     await browser.close();
 })();
